112e4754664b5015ecf9f09663fd916fe733c5d3
yesntface <i_lekishvili@cu.edu.ge>
2021-12-12 23:30:35 +0400

    anim

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
 const scene = document.querySelector('.scene');
-const img = document.querySelector('.scene>img');
-const imageCount = 125;
+const animation = document.querySelector('#ufo__animation');
+const background = document.querySelector('#ufo__background');
+const animationCount = 118;
+const backgroundCount = 13;
 let imageLoaded = 0;
-const urls = new Array(imageCount).fill("").map((_, i) => `./img/UFO_20Animation_atlas_${i + 1}.png`);
+const animationUrls = new Array(animationCount).fill("").map((_, i) => `./img/animation/UFO_20Animation_atlas_${i + 1}.png`);
+const backgroundUrls = new Array(backgroundCount).fill("").map((_, i) => `./img/background/ufo_background_${i + 1}.png`);
 
-urls.forEach(url => {
+animationUrls.forEach(url => {
     const img = new Image();
     img.src = url;
     img.onload = () => {
         imageLoaded++;
-        if (imageLoaded === imageCount) {
+        if (imageLoaded === animationUrls.length + backgroundUrls.length) {
             init();
         }
-        // scene.appendChild(img);
+    }
+});
+
+backgroundUrls.forEach(url => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => {
+        imageLoaded++;
+        if (imageLoaded === animationUrls.length + backgroundUrls.length) {
+            init();
+        }
     }
 });
 
 function init() {
-    let index = 0;
-    setInterval(() => {
-        img.src = urls[index];
-        index++;
-        if(index === imageCount) {
-            index = 0;
+    let position = 0;
+    let ufoIndex = 0;
+    let backgroundIndex = 0;
+
+    const interval = setInterval(() => {
+        
+
+        if (ufoIndex === 118) {
+            background.classList.remove('hidden');
+            animation.classList.add('hidden');
         }
+        if(ufoIndex < 118) {
+            animation.src = animationUrls[ufoIndex];
+            ufoIndex++;
+        }
+
+
+        if(ufoIndex === 118) {
+
+            background.style.backgroundImage = `url(${backgroundUrls[backgroundIndex]})`;
+            backgroundIndex++;
+        }
+
+        if (backgroundIndex === backgroundUrls.length) {
+            background.classList.add('hidden');
+            clearInterval(interval);
+        }
+
     }, 1000 / 30)
 }
 
